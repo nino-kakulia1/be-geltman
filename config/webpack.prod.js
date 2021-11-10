41165c57b74d680c491f18d2bcb5b0151bf8eadd
nino-kakulia1 <78740756+nino-kakulia1@users.noreply.github.com>
2021-11-10 08:13:50 -0900

    turn off module for css lodader

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -24,7 +24,7 @@ module.exports = merge(common, {
             options: {
               importLoaders: 2,
               sourceMap: false,
-              modules: true,
+              modules: false,
             },
           },
           'postcss-loader',
